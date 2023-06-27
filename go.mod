07a6f97a1bfdb3814165c45837bf4ebbaa78d1f4
kucicm <10778553+KucicM@users.noreply.github.com>
2023-06-28 01:08:06 +0200

    init test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kucicm/anagrab
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func main() {
+	js.Global().Set("reverseText", js.FuncOf(reverseText))
+	select {}
+}
+
+func reverseText(this js.Value, args []js.Value) interface{} {
+	doc := js.Global().Get("document")
+	textbox := doc.Call("getElementById", "textbox")
+	label := doc.Call("getElementById", "label")
+
+	text := textbox.Get("value").String()
+	reversed := reverseString(text)
+
+	label.Set("textContent", reversed)
+	return nil
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}

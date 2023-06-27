33ca641e30591dd32083964c181d13167c9c5b9c
kucicm <10778553+KucicM@users.noreply.github.com>
2023-06-28 01:55:07 +0200

    not working?

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func reverseText(this js.Value, args []js.Value) interface{} {
 
 func reverseString(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+2, j-2 {
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)

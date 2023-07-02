32654130044a40450853556c6263028030f186c6
kucicm <10778553+KucicM@users.noreply.github.com>
2023-07-02 17:28:01 +0200

    remove manual compression

--- data/build.py
+++ data/build.py
@@ -1,9 +1,6 @@
 import argparse
 import string
-import heapq
 import pathlib
-from collections import Counter
-from typing import Dict, Tuple
 
 ASCII_LETTERS = set(string.ascii_letters)
 END_CHAR = "!"
@@ -28,35 +25,6 @@ def is_ascii_word(word: str) -> bool:
     return all(w in ASCII_LETTERS for w in word.strip())
 
 
-def build_compression_tree(words: str) -> Tuple[Dict[str, bytes], Dict[bytes, str]]:
-    print("building compression tree")
-    nodes = [(f, i, w) for i, (w, f) in enumerate(Counter(words).items())]
-    nodes.append((1, -1, END_CHAR))
-    heapq.heapify(nodes)
-
-    while len(nodes) > 1:
-        f1, i, n1 = heapq.heappop(nodes)
-        f2, i, n2 = heapq.heappop(nodes)
-        heapq.heappush(nodes, (f1 + f2, i, (n1, n2)))
-
-    def build(node, tree: Dict[str, bytes], b=b""):
-        if isinstance(node, str):
-            tree[node] = b
-            return
-        build(node[0], tree, b + b"0")
-        build(node[1], tree, b + b"1")
-
-    tree: Dict[str, bytes] = dict()
-    build(nodes[0][2], tree)
-    return tree, {v: k for k, v in tree.items()}
-
-
-def compress_words(word: str, tree: Dict[str, bytes]) -> bytes:
-    print("compressing")
-    encoded = [tree[c] for c in word + f"\n{END_CHAR}"]
-    return b"".join(encoded)
-
-
 def to_bin(bins: bytes):
     return repr(bins)[2:-1]
 
@@ -72,14 +40,3 @@ if __name__ == "__main__":
     out_path = pathlib.Path(args.output_file_path)
     with open(out_path / "word_list.txt", "w") as f:
         f.write(words)
-
-    encode_tree, decode_tree = build_compression_tree(words)
-    with open(out_path / "compression_tree.txt", "w") as f:
-        parts = "|".join(["|".join([to_bin(k), v]) for k, v in decode_tree.items()])
-        f.write(parts)
-
-    e = compress_words(words, encode_tree)
-    with open(out_path / "word_list.bin", "wb") as f:
-        for i in range(0, len(e), 8):
-            byte = e[i:i+8] + b"0" * (8 - len(e[i:i+8]))
-            f.write(int(byte, 2).to_bytes(1, "big"))

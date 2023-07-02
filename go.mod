fd860e57cac299b087029339b7805f5231edfbaf
kucicm <10778553+KucicM@users.noreply.github.com>
2023-07-02 12:41:17 +0200

    prep

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kucicm/anagrab
 
-go 1.18
+go 1.20
--- src/main.go
+++ src/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"syscall/js"
+    "syscall/js"
 )
 
 const ENTER_KEY_CODE = 13
 
 func main() {
-	js.Global().Set("handleKeyPress", js.FuncOf(handleKeyPress))
-    
+    js.Global().Set("handleKeyPress", js.FuncOf(handleKeyPress))
+
     c := make(chan struct{})
     c <- struct{}{}
 }
@@ -18,9 +18,9 @@ func handleKeyPress(this js.Value, args []js.Value) interface{} {
     event := args[0]
     keyCode := event.Get("keyCode").Int()
     if keyCode == ENTER_KEY_CODE {
-        fmt.Println("Enter")
+        fmt.Println("ENTER")
     }
     fmt.Printf("%d\n", keyCode)
+
     return nil
 }
-
